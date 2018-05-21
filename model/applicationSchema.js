46fc54c27987c413694240cc19a5acc95e0429cf
KmKavya <kavyavshaiva@gmail.com>
2018-05-21 17:30:34 +0530

    changed filenames

---
+++ model/applicationSchema.js
@@ -0,0 +1,52 @@
+// JSON schema representation of application  module
+
+module.exports.schema = {
+  "$schema": "http://json-schema.org/draft-06/schema#",
+  "title": "applicationModel",
+  "type": "object",
+  "properties": {
+    "applicationId": {
+      "type": "number"
+    },
+    "applicationCode": {
+      "type": "string",
+      "minLength": 1,
+      "maxLength": 4
+    },
+    "applicationName": {
+      "type": "string",
+      "minLength": 1,
+      "maxLength": 100
+    },
+    "enabled": {
+      "type": "boolean",
+      "default": true
+    },
+    "logo": {
+      "type": "string"
+    },
+    "favicon": {
+      "type": "string"
+    },
+    "createdBy": {
+      "type": "string"
+    },
+    "updatedBy": {
+      "type": ["string", "null"]
+    },
+    "createdDate": {
+      "type": "string",
+      "format": "date-time"
+    },
+    "updatedDate": {
+      "type": ["string", "null"],
+      "format": "date-time"
+    },
+    "description": {
+      "type": "string",
+      "minLength": 0,
+      "maxLength": 255
+    }
+  },
+  "required": ["applicationId", "applicationCode", "applicationName", "createdBy", "createdDate"]
+};
